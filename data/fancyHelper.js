5e6b6a18e8fc2ea57f3e39ab90896ba69ef5033e
Jonathon Harding <git@jrhardin.net>
2012-06-30 12:58:51 -0400

    YOSPOS Stylesheet fix
    The CSS attach function now checks for the body to exist - meaning the
    entire head must be loaded I think. This makes sure that 219.css is
    correctly identified.
    
    Version incremented to 0.2.1.

--- data/fancyHelper.js
+++ data/fancyHelper.js
@@ -80,11 +80,16 @@ fancySAHelper = {
   
   AttachCSS: function(count) {
 	self.port.emit("log", "AttachCSS started");
+  bod = $("body#something_awful");
 	css = $("link[rel=stylesheet][href$='globalcss/globalmenu.css']");
 	if (css.size() === 0) {
 		css = $("link[rel=stylesheet][href='/aw/css/core.min.css']");
 	}
-	if (css.size() > 0) {
+	if (bod.size() > 0) {
+    css = $("link[rel=stylesheet][href$='globalcss/globalmenu.css']");
+    if (css.size() === 0) {
+      css = $("link[rel=stylesheet][href='/aw/css/core.min.css']");
+    }
 		// Attach a bit of CSS that makes things look good before the script finishes
 		$(css).after("<style type='text/css'> body > #globalmenu { margin: 0 auto !important; } #content > div.pages, #content > #ac_timemachine { display: none; } </style>");
 		// And attach the main fancy.css
